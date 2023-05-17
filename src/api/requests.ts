ae2ece8bd8915d23aeff51ace519cb6c82696ccd
Williams Samuel <samwill300@gmail.com>
2023-05-17 16:50:27 +0100

    first commit

---
+++ src/api/requests.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosError } from 'axios'
+import { getAccessToken, removeAccessToken } from 'utils/storage'
+
+export const baseURL = 'https://stage.wustomers.com/api/v1'
+
+export const instance = axios.create({
+	baseURL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+})
+
+// intercepts private requests and add token to header
+instance.interceptors.request.use(
+	async config => {
+		const token = getAccessToken()
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`
+		}
+
+		return config
+	},
+	error => Promise.reject(error)
+)
+
+// intercepts private response and check if token has expired
+instance.interceptors.response.use(
+	response => {
+		return response
+	},
+	error => {
+		if (error instanceof AxiosError) {
+			if (error.response?.status === 401) {
+				removeAccessToken()
+				// window.location.pathname = '/login'
+			}
+
+			throw error
+		}
+	}
+)
